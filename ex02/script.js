2e942c07a2cdcd650283587979acd5f643421f70
LuciaraPereira <luciarapereir4@gmail.com>
2025-02-05 20:38:51 -0300

    ex02

---
+++ ex02/script.js
@@ -0,0 +1,10 @@
+/*alert('olá')
+confirm('confirme')
+prompt('Seu nome:')
+let nome = prompt('Digite seu nome:')
+alert('Olá '+nome)*/
+
+/*Faça o algoritmo (diagrama de blocos e português estruturado) para calcular o valor monetário do
+desconto (D), sendo que o preço do produto (PR) é fornecido pelo usuário e o desconto tambem.
+Apresentar o valor do desconto. */
+
