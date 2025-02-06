e7d9d4288b2e6573715c96df0f3699e663295d18
LuciaraPereira <luciarapereir4@gmail.com>
2025-02-06 11:34:13 -0300

    exercicio 2

--- ex02/script.js
+++ ex02/script.js
@@ -4,7 +4,20 @@ prompt('Seu nome:')
 let nome = prompt('Digite seu nome:')
 alert('Olá '+nome)*/
 
-/*Faça o algoritmo (diagrama de blocos e português estruturado) para calcular o valor monetário do
+/*Faça o algoritmo para calcular o valor monetário do
 desconto (D), sendo que o preço do produto (PR) é fornecido pelo usuário e o desconto tambem.
 Apresentar o valor do desconto. */
+
+let preco =  document.getElementById('preco');
+let desconto = document.getElementById('desconto');
+let resultado = document.querySelector('.resultado');
+
+
+function calcularDesconto(){
+    let valorPreco = Number(preco.value);
+    let valorDesconto = Number(desconto.value);
+    let calculoDesconto = valorPreco * (valorDesconto/100);
+    let precoFinalDesconto = valorPreco - calculoDesconto;
+   resultado.innerHTML = `O desconto é de R$${calculoDesconto} e o valor do produto com desconto fica R$${precoFinalDesconto}` 
+}
 
