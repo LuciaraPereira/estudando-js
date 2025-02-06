24d7363e3a22e86b5f59298bbd64d3161ae829b4
LuciaraPereira <luciarapereir4@gmail.com>
2025-02-06 20:32:16 -0300

    ajustes ex 3

--- ex03/script.js
+++ ex03/script.js
@@ -4,35 +4,44 @@ deseja continuar entrando com valores.
 OBS: Não se esqueça de validar a resposta do usuário, pois ele só pode responder “S” ou “N”. */
 
 let numbers = [];
+let resultado = document.getElementById('resultado')
+let numero = document.getElementById('numero');
 
 function verificar(){
-
-    let resp 
+    let resp = ''
    do{ 
-    let numero = document.getElementById('numero');
-    numbers.push(numero.value);
-
-    console.log(numbers)
-
-    limparNumber()
-   
+    numbers.push(parseInt(numero.value));
     let resp = prompt('Digite "S" para continuar ou "N" para parar')
+    limparNumber()
+    listandoNumeros()
 
-    if(resp === 'n'){
-        let resultado = document.getElementById('resultado')
-        resultado.innerHTML="foi n"
+    if(resp === 'N' || resp === 'n'){
+        verificarTamanho()
     }
-    resp++
-    } while (resp === 'S' || resp === 's');
-
-   
-    
-}
-
-function listandoNumbers(){
-    
+    } while (resp === 'S' || resp === 's');    
 }
 
 function limparNumber(){
     numero.value=''
+}
+
+function verificarTamanho(){
+    let maior = numbers[0]
+    let menor = numbers[0]
+
+    for(let i = 1; i < numbers.length; i++ )
+    if(numbers[i] > maior){
+        maior = numbers[i]
+       
+    } else if(numbers[i] < menor){
+        menor= numbers[i]
+    }
+    resultado.innerHTML = `O maior número é: ${maior} e o menor é: ${menor}`
+}
+
+function listandoNumeros(){
+    let lista = document.getElementById('listaNumeros')
+    let adiciona = document.createElement("li")
+    adiciona.innerHTML= numbers[numbers.length - 1]
+    lista.appendChild(adiciona)
 }
