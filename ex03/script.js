873a1230131e28c32e14a36b38841652e069c92a
LuciaraPereira <luciarapereir4@gmail.com>
2025-02-06 18:39:33 -0300

    ex 3

--- ex03/script.js
+++ ex03/script.js
@@ -2,3 +2,37 @@
 valor fornecido. Para cada valor digitado, deve ser solicitado ao usuário que ele digite se ele
 deseja continuar entrando com valores.
 OBS: Não se esqueça de validar a resposta do usuário, pois ele só pode responder “S” ou “N”. */
+
+let numbers = [];
+
+function verificar(){
+
+    let resp 
+   do{ 
+    let numero = document.getElementById('numero');
+    numbers.push(numero.value);
+
+    console.log(numbers)
+
+    limparNumber()
+   
+    let resp = prompt('Digite "S" para continuar ou "N" para parar')
+
+    if(resp === 'n'){
+        let resultado = document.getElementById('resultado')
+        resultado.innerHTML="foi n"
+    }
+    resp++
+    } while (resp === 'S' || resp === 's');
+
+   
+    
+}
+
+function listandoNumbers(){
+    
+}
+
+function limparNumber(){
+    numero.value=''
+}
