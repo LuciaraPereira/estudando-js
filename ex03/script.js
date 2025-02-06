4c3b190923390fb4d198845862fef4aa3b0396d1
LuciaraPereira <luciarapereir4@gmail.com>
2025-02-06 12:03:20 -0300

    exercicio 3

---
+++ ex03/script.js
@@ -0,0 +1,4 @@
+/* Construa um programa que leia vários números inteiros e mostre qual foi o maior e o menor
+valor fornecido. Para cada valor digitado, deve ser solicitado ao usuário que ele digite se ele
+deseja continuar entrando com valores.
+OBS: Não se esqueça de validar a resposta do usuário, pois ele só pode responder “S” ou “N”. */
