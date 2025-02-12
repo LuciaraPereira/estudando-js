4e26ff6d1a89d0ce07c83b8f186cbe46bddd453e
LuciaraPereira <luciarapereir4@gmail.com>
2025-02-11 21:13:14 -0300

    ex 5

---
+++ ex05/script.js
@@ -0,0 +1,4 @@
+/*Fazer um algoritmo para ajudar a bilheteria do metrô. O operador deve informar o tipo do bilhete
+(unitário, duplo ou 10 viagens) e o valor pago pelo passageiro. O sistema deve mostrar, então, a
+quantidade de bilhetes possíveis e o troco que o passageiro deve receber. Considere a seguinte
+tabela de preço: Bilhete unitário 5,30 Bilhete duplo 10,60 Bilhete de 10 viagens 52,00 */
